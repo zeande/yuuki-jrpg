596bc8c0666680bc5f3bceb9dc93a1b303c537e4
Caleb Smith <caleb.smith@ndsu.edu>
2012-10-03 10:41:46 -0500

    Update for PlayerName Class

--- src/yuuki/PlayerName.java
+++ src/yuuki/PlayerName.java
@@ -1,12 +1,22 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
+//Imports.
+import java.util.Scanner;
 /**
- *
+ *Class PlayerName contains a method that prompts the user to enter a name
+ * for their (player style) character.
  * @author Caleb
  */
 public class PlayerName {
     
+    public String playerName()
+    {
+        //Declare Variables.
+        String name = "Player";
+        //Instantiates an instance of Scanner.
+        Scanner scan = new Scanner(System.in);
+        
+        //Prompt user for a character name.
+        System.out.println("Please enter a name for your charater:");
+        name = scan.nextLine();
+        return name;
+    }
 }
