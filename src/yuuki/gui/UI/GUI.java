6ee1af9eb9b256b6f7c9a3d87117756d80da3b02
Caleb Smith <caleb.smith@ndsu.edu>
2012-10-22 22:00:10 -0500

    GUI Update (whole package included)
    Added Options Menu. Cleaned comments and code.

---
+++ src/yuuki/gui/UI/GUI.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui.UI;
+
+/**
+ *
+ * @author Caleb
+ */
+public class GUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainTitle mt = new MainTitle();
+        mt.setVisible(true);
+    }
+}
